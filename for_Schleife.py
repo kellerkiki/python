e954ad786ecdb62725990040a90089068b4b8b77
Christian Laskawi <Christian.Laskawi@crissy.de>
2019-08-24 12:10:21 +0200

    for Schleifen tests

---
+++ for_Schleife.py
@@ -0,0 +1,19 @@
+#erste for test
+edibles = ["ham", "pups", "eggs","nuts"]
+for food in edibles:
+    if food == "pups":
+        print("Found pups, no more pups please!")
+        continue
+    print("Great, delicious " + food)
+    # here can be the code for enjoying our food :-)
+else:
+    print("I am so glad: No pups!")
+print("Finally, I finished stuffing myself")
+
+
+# zweiter for test
+liste = ["eins","zwei","drei"]
+for printer in liste:
+    print (printer)
+
+
--- while-Schleife.py
+++ while-Schleife.py
@@ -1,6 +1,10 @@
 x = 0
+
 while(x < 10):
     print(x)
     x += 1
 else: print("x ist jetzt nicht mehr kleiner als 10")
+
+
+
 
